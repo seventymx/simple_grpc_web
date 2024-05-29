f46197acf7c0062d4939cd340d99c12f65a3fe06
Steffen70 <steffen@seventy.mx>
2024-05-29 12:46:35 +0200

    Added solution of candidate A.

--- backend/SPTrialWebServiceCS/Services/NavService.cs
+++ backend/SPTrialWebServiceCS/Services/NavService.cs
@@ -1,5 +1,7 @@
-﻿using Google.Protobuf.WellKnownTypes;
+﻿using System.Data;
+using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
+using Microsoft.Data.SqlClient;
 using SwissPension.Trial.Common;
 
 namespace SwissPension.Trial.Tree.Services;
@@ -10,28 +12,56 @@ public class NavService : Nav.NavBase
 
     public override async Task GetStiftungenStream(Empty request, IServerStreamWriter<NavData> responseStream, ServerCallContext context)
     {
-        var stiftungen = new List<NavData>
+        //var stiftungen = new List<NavData>
+        //{
+        //    new NavData
+        //    {
+        //        Id = 1,
+        //        Text = "Stiftung 1",
+        //        ImageName = ResourceAccessor.ResourceName.stiftung.ToString()
+        //    },
+        //    new NavData
+        //    {
+        //        Id = 2,
+        //        Text = "Stiftung 2",
+        //        ImageName = ResourceAccessor.ResourceName.stiftung.ToString()
+        //    },
+        //    new NavData
+        //    {
+        //        Id = 3,
+        //        Text = "Stiftung 3",
+        //        ImageName = ResourceAccessor.ResourceName.stiftung.ToString()
+        //    }
+        //};
+        var stiftungen = new List<NavData>();
+
+        using (SqlConnection connection = new SqlConnection("Server=localhost;Database=SP7;Trusted_Connection=yes;Connection Timeout=60;Encrypt=False;"))
         {
-            new NavData
+            await connection.OpenAsync();
+            using (SqlCommand command = new SqlCommand("SELECT STID AS ID, STName AS Text FROM Stiftungen WHERE STID > 0", connection))
             {
-                Id = 1,
-                Text = "Stiftung 1",
-                ImageName = ResourceAccessor.ResourceName.stiftung.ToString()
-            },
-            new NavData
-            {
-                Id = 2,
-                Text = "Stiftung 2",
-                ImageName = ResourceAccessor.ResourceName.stiftung.ToString()
-            },
-            new NavData
-            {
-                Id = 3,
-                Text = "Stiftung 3",
-                ImageName = ResourceAccessor.ResourceName.stiftung.ToString()
+                using (SqlDataReader reader = await command.ExecuteReaderAsync(CommandBehavior.SequentialAccess))
+                {
+                    while (await reader.ReadAsync())
+                    {
+                        var navData = new NavData();
+
+                        if (!(await reader.IsDBNullAsync(0)))
+                        {
+                            navData.Id = reader.GetInt32(0); // Assuming ID is an integer
+                        }
+
+                        if (!(await reader.IsDBNullAsync(1)))
+                        {
+                            navData.Text = reader.GetString(1); // Assuming Text is a string
+                        }
+
+                        stiftungen.Add(navData);
+                    }
+                }
             }
-        };
-        
+        }
+
         foreach (var stiftung in stiftungen)
         {
             await responseStream.WriteAsync(stiftung);
